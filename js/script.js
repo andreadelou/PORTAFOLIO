d6e84f8749fb095d7fbeeaa59744a58fd6fe10a8
andreadelou <70552370+andreadelou@users.noreply.github.com>
2022-06-04 19:11:03 -0600

    home

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
-let menu = document.querySelector('#menu-bars');
+
 let header = document.querySelector('header');
 
-// Boton superior que quita menu
-menu.onClick = () =>{
-    menu.classList.toggle('fa-times');
-    header.classList.toggle('active');
-}
+
 
 window.onscroll = () =>{
     menu.classList.remove('fa-times');
